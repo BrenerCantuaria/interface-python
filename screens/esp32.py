ea07a88edcba5cb1af9ddd4773daae798e998f2d
Márcio Brener <97567157+BrenerCantuaria@users.noreply.github.com>
2024-10-25 20:06:13 -0300

    update: simulando bloqueo de botão se já tiver ID

--- screens/esp32.py
+++ screens/esp32.py
@@ -1,14 +1,19 @@
+import random
 import tkinter as tk
 from tkinter import ttk, messagebox
 import tkinter as tk
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 from ttkbootstrap.style import Style
-
 from .centraliza_tela import center_window
 from .config_screens import window_height,window_width
 
-import random
+# Simulando a função que verifica se já existe um ID na ESP32
+def get_existing_id():
+    # Esta função deve interagir com o hardware ESP32 para obter o ID existente
+    # Aqui está simulado como se sempre houvesse um ID já existente
+    return None # Simula um ID existente
+
 
 def abrir_janela_esp32(root):
     janela_esp32 = tk.Toplevel(root)
@@ -18,6 +23,10 @@ def abrir_janela_esp32(root):
     
     # Variável para armazenar o ID gerado
     id_var = tk.StringVar(value="")
+
+    existing_id = get_existing_id()
+    if existing_id:
+        id_var.set(existing_id) 
 
     # Container principal para os widgets centralizados verticalmente
     container = ttk.Frame(janela_esp32)
@@ -32,10 +41,10 @@ def abrir_janela_esp32(root):
     button_container.pack(pady=10)
 
     def generate_id():
-        # Gera um novo ID
-        id_generated = f"{random.randint(100,999)}-{random.randint(100,999)}-{random.randint(100,999)}"
-        id_var.set(id_generated)
-        save_button.pack(side='left', padx=10)  # Mostra o botão Gravar
+        if not existing_id:
+            id_generated = f"{random.randint(100,999)}-{random.randint(100,999)}-{random.randint(100,999)}"
+            id_var.set(id_generated)
+            save_button.pack(side='left', padx=10)  # Mostra o botão Gravar
 
     generate_button = ttk.Button(button_container, text="Gerar ID", command=generate_id)
     generate_button.pack(side='left', padx=10)
