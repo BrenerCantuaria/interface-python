631ee6352d91cb74166bd4a6ab8afba93b59343c
Márcio Brener <97567157+BrenerCantuaria@users.noreply.github.com>
2024-10-25 19:21:30 -0300

    refactor: melhorando componentes na tela

--- screens/esp32.py
+++ screens/esp32.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import Label, Button, StringVar, Frame, Entry
+from tkinter import ttk, messagebox
 from .centraliza_tela import center_window
 from .config_screens import window_height,window_width
 
@@ -10,29 +10,39 @@ def abrir_janela_esp32(root):
     janela_esp32.title("ESP32")
     center_window(janela_esp32, window_width, window_height)
     janela_esp32.resizable(False, False)
-    manage_esp32_id(janela_esp32)
 
-def manage_esp32_id(window):
-    esp32_id = None
-    id_var = StringVar()
-    container = tk.Frame(window)
-    container.pack(padx=20, pady=20)
+    
+    # Variável para armazenar o ID gerado
+    id_var = tk.StringVar(value="")
 
-    id_label = Label(container, text="ID:", font=('Helvetica', 12))
-    id_label.grid(row=0, column=0, pady=(0, 20))
+    # Container principal para os widgets centralizados verticalmente
+    container = ttk.Frame(janela_esp32)
+    container.place(relx=0.5, rely=0.5, anchor='center')  # Posiciona no centro da janela
 
-    entry_id = Entry(container, textvariable=id_var, width=30)
-    entry_id.grid(row=0, column=1)
+    # Campo de entrada de ID
+    entry_id = ttk.Entry(container, textvariable=id_var, width=40)
+    entry_id.pack(ipady=10, pady=20)  # Aumenta o padding interno para maior altura
+
+    # Botões gerar e gravar ID abaixo do campo de entrada
+    button_container = ttk.Frame(container)
+    button_container.pack(pady=10)
 
     def generate_id():
-        nonlocal esp32_id
-        esp32_id = f"{random.randint(100,999)}-{random.randint(100,999)}-{random.randint(100,999)}"
-        id_var.set(esp32_id)
-        generate_button.pack_forget()
-        save_button.pack()
+        # Gera um novo ID
+        id_generated = f"{random.randint(100,999)}-{random.randint(100,999)}-{random.randint(100,999)}"
+        id_var.set(id_generated)
+        save_button.pack(side='left', padx=10)  # Mostra o botão Gravar
 
-    generate_button = Button(container, text="Gerar ID", command=generate_id, font=('Helvetica', 12))
-    generate_button.grid(row=0, column=2, padx=10)
+    generate_button = ttk.Button(button_container, text="Gerar ID", command=generate_id)
+    generate_button.pack(side='left', padx=10)
 
-    save_button = Button(window, text="Gravar ID", font=('Helvetica', 12))
-    save_button.pack_forget()  # Inicialmente oculto
+    def save_id():
+        # Grava o ID gerado
+        if id_var.get():
+            messagebox.showinfo("Sucesso", "ID gravado com sucesso na ESP32!")
+        else:
+            messagebox.showerror("Falha", "Falha ao gravar ID.")
+        save_button.pack_forget()  # Oculta o botão Gravar
+
+    save_button = ttk.Button(button_container, text="Gravar ID", command=save_id)
+    save_button.pack_forget()  # Oculto até um ID ser gerado
