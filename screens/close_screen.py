41dcafbb269d6f677737777015335dcd85c74b2e
Márcio Brener <97567157+BrenerCantuaria@users.noreply.github.com>
2024-11-05 20:49:10 -0300

    update: removendo método voltar_tela_principal

--- screens/close_screen.py
+++ screens/close_screen.py
@@ -1,9 +1,26 @@
 import tkinter as tk
+from tkinter import messagebox
 
 class CloseScreen:
-    
-    def voltar_tela_principal(tela):
-        tela.destroy()
-        tela.deiconify()  # Mostra a tela principal novamente
+    def __init__(self, root):
+        self.root = root  # Armazena uma referência à janela principal
 
-   
+    def close_window(self, window):
+        """
+        Fecha a janela especificada e opcionalmente realiza outras ações.
+        """
+        window.destroy()  # Destroi a janela passada como argumento
+
+    def confirm_and_close_window(self, window, message="Deseja realmente fechar?"):
+        """
+        Exibe uma caixa de diálogo de confirmação antes de fechar a janela.
+        """
+        if messagebox.askyesno("Confirmar", message):
+            self.close_window(window)  # Fecha a janela se o usuário confirmar
+
+    def close_and_return(self, window):
+        """
+        Fecha a janela atual e retorna para a tela principal.
+        """
+        self.close_window(window)
+        self.root.deiconify()  # Traz a janela principal de volta ao foco
