c637d1b8478a74186b8b24c920035285161ca799
Márcio Brener <97567157+BrenerCantuaria@users.noreply.github.com>
2024-11-05 21:14:15 -0300

    delete: deletando comentários

--- screens/arduino.py
+++ screens/arduino.py
@@ -1,12 +1,3 @@
-# import tkinter as tk
-# from .centraliza_tela import center_window
-# from .config_screens import window_height,window_width
-
-# def abrir_janela_arduino(root):
-#     janela_arduino = tk.Toplevel(root)
-#     janela_arduino.title("Arduino")
-#     center_window(janela_arduino, window_width, window_height)
-#     tk.Label(janela_arduino, text="Arduino").pack()
 import tkinter as tk
 from tkinter import ttk
 from .centraliza_tela import center_window
