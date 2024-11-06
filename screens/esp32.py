e9f6e68db4e79a1b1885b448879711fa66d96eb8
Márcio Brener <97567157+BrenerCantuaria@users.noreply.github.com>
2024-11-05 22:09:57 -0300

    refactor: add variáveis globais de conexão e função para mudar a conexão
    Nessa refatoração eu adicionei uma variável global para conexão, e também a criei a função on_port_selected, para alterar a porta.

--- screens/esp32.py
+++ screens/esp32.py
@@ -1,15 +1,14 @@
 import random
 import tkinter as tk
 from tkinter import ttk, messagebox
-import tkinter as tk
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 from ttkbootstrap.style import Style
 from .centraliza_tela import center_window
 from .config_screens import window_height, window_width
-from service.serial import get_existing_id
-from service.serial import send_device_info, open_serial_connection
+from service.serial import send_device_info, open_serial_connection, get_existing_data
 from screens.close_screen import CloseScreen
+
 
 def abrir_janela_esp32(root):
     root.withdraw()
@@ -20,94 +19,90 @@ def abrir_janela_esp32(root):
     janela_esp32.grab_set()
     fecha_janela = CloseScreen(root=root)
 
-    
-    
-    # Variável para armazenar o informações
+    # Variáveis para armazenar informações
     id_var = tk.StringVar(value="")
     nome_dispositivo = tk.StringVar(value="")
 
-    existing_id = get_existing_id()
-    if existing_id:
-        print("ID existente:", existing_id[0]['id'])  # Debug
-        print("Nome do dispositivo:",
-              existing_id[0]['nome_dispositivo'])  # Debug
-        id_var.set(existing_id[0]['id'])
-        nome_dispositivo.set(existing_id[0]['nome_dispositivo'])
+    # Conexão serial global
+    global serial_connection
+    serial_connection = open_serial_connection('COM4')  # Assume que retorna um tuple (connection, status)
+    print(serial_connection)
+    if not serial_connection:
+        messagebox.showerror("Erro Serial", "Não foi possível abrir a conexão serial na porta padrão. COM3")
 
-    # Elementos em tela
+    
 
-    # Container principal para os widgets centralizados verticalmente
+    # Obter dados existentes do dispositivo
+    existing_data = get_existing_data(True)
+    can_generate_id = True  # Presume que pode gerar um novo ID por padrão
+    if existing_data:
+        nome_dispositivo.set(existing_data['nome'])
+        id_var.set(existing_data['id'])
+        can_generate_id = False  # Desativa a geração de novo ID se já existir
+
+    # Container principal para os widgets
     container = ttk.Frame(janela_esp32)
-    # Posiciona no centro da janela
     container.place(relx=0.5, rely=0.5, anchor='center')
 
+    # Combobox para seleção de porta serial
     label_port = ttk.Label(container, text="Selecione a porta serial:")
     label_port.pack(pady=(10, 0))
-
-    port_combobox = ttk.Combobox(container, width=37)
-    port_combobox['values'] = ['COM3', 'COM4', 'COM5']
+    port_combobox = ttk.Combobox(container, width=37, values=[
+                                 'COM3', 'COM4', 'COM5'])
     port_combobox.pack(pady=10)
-    port_combobox.current(0)
-
-    # Adiciona um observador para mudanças no Combobox
+    port_combobox.set('COM4')  # Define a porta padrão ou a última usada
+    
     def on_port_selected(event):
+        global serial_connection
         selected_port = port_combobox.get()
-        print(f"Porta selecionada: {selected_port}")
-        open_serial_connection(selected_port)
+        new_serial_connection, success = open_serial_connection(selected_port)
+        if success:
+            serial_connection = new_serial_connection  # Atualiza a conexão serial
+            messagebox.showinfo("Conexão Serial", f"Conexão estabelecida com sucesso na porta {selected_port}.")
+        else:
+            messagebox.showerror("Conexão Serial", f"Falha ao estabelecer conexão na porta {selected_port}.")
 
     port_combobox.bind("<<ComboboxSelected>>", on_port_selected)
 
-    # Campo de entrada do nome da ESP32
+    # Campo de entrada do nome do dispositivo
     label_name = ttk.Label(
-        container, text="Nome do dispositivo: ", font=("Helvetica", 14))
+        container, text="Nome do dispositivo:", font=("Helvetica", 14))
     label_name.pack(pady=(10, 0))
-    entry_name = tk.Entry(container, textvariable=nome_dispositivo, width=40)
+    entry_name = ttk.Entry(container, textvariable=nome_dispositivo, width=40)
     entry_name.pack(ipady=10, pady=10)
 
     # Campo de entrada de ID
-    label_id = ttk.Label(container, text="ID: ", font=("Helvetica", 14))
+    label_id = ttk.Label(container, text="ID:", font=("Helvetica", 14))
     label_id.pack(pady=(10, 0))
-    entry_id = ttk.Entry(container, textvariable=id_var, width=40)
+    entry_id = ttk.Entry(container, textvariable=id_var, width=40,
+                         state='normal' if can_generate_id else 'disabled')
     entry_id.pack(ipady=10, pady=20)
 
-    # Botões gerar e gravar ID abaixo do campo de entrada
-    button_container = ttk.Frame(container)
-    button_container.pack(pady=10)
-
-    def generate_id():
-        if existing_id[0]['id'] == None:
-            id_generated = f"{random.randint(100,999)}-{random.randint(100,999)}-{random.randint(100,999)}"
-            id_var.set(id_generated)
-            save_button.pack(side='left', padx=10)  # Mostra o botão Gravar
-
+    # Botão para gerar um novo ID
     generate_button = ttk.Button(
-        button_container, text="Gerar ID", command=generate_id)
-    generate_button.pack(side='left', padx=10)
+        container, text="Gerar ID", command=lambda: generate_id(id_var))
+    if can_generate_id:
+        generate_button.pack(side='left', padx=10)
 
-    def save_id():
-        # Grava o ID gerado
-        if id_var.get() and nome_dispositivo.get() != "":
-            send_device_info(True, nome_dispositivo, id_var)
+    # Botão para salvar as alterações
+    save_button = ttk.Button(container, text="Salvar alterações", command=lambda: save_id(
+        serial_connection, nome_dispositivo.get(), id_var.get()))
+    save_button.pack(side='bottom', pady=20)
+
+
+    def generate_id(id_var):
+        id_generated = f"{random.randint(100,999)}-{random.randint(100,999)}-{random.randint(100,999)}"
+        id_var.set(id_generated)
+
+
+    def save_id(serial_connection, name, device_id):
+        if name and device_id:
+            send_device_info(serial_connection, name, device_id)
             messagebox.showinfo(
-                "Sucesso", f"ID:{id_var.get()} gravado com sucesso na ESP32!")
+            "Sucesso", f"ID: {device_id} e nome: {name} salvos com sucesso!")
         else:
             messagebox.showerror(
-                "Falha", "Falha ao gravar ID, o campo nome não pode estar vazio.")
-        save_button.pack_forget()  # Oculta o botão Gravar
-
-    def save_database():
-        messagebox.showinfo("Salvar", "Alterações salvas no banco de dados")
-
-   
-
-
-    #Ação de botões 
-    save_button = ttk.Button(
-        button_container, text="Gravar ID", command=save_id)
-    save_button.pack_forget()  # Oculto até um ID ser gerado
-
-    salvar_alteracoes = ttk.Button(janela_esp32, text="Salvar alterações", command=save_database)
-    salvar_alteracoes.pack(side='bottom', pady=20)  # Posiciona no fundo da janela
+            "Erro", "Nome do dispositivo e ID são necessários.")
 
     # Fechamento da tela
     voltar_button = tk.Button(janela_esp32, text="← Voltar",  command=lambda: fecha_janela.close_and_return(janela_esp32))
