be0694f351a9461804178db176327a9559b55e14
Márcio Brener <97567157+BrenerCantuaria@users.noreply.github.com>
2024-11-23 20:06:42 -0300

    Update arduino.py

--- screens/arduino.py
+++ screens/arduino.py
@@ -8,10 +8,8 @@ from screens.close_screen import CloseScreen
 # Variável global para a conexão serial
 serial_connection_arduino = None
 
-
 def get_dispositivos_eps32():
     return ["ESP32 Dev Kit", "ESP32 WROOM", "ESP32 WROVER"]
-
 
 def abrir_janela_arduino(root):
     global serial_connection_arduino
@@ -33,48 +31,19 @@ def abrir_janela_arduino(root):
     dispositivos = get_dispositivos_eps32()
 
     # Combobox para selecionar dispositivos ESP32
-    label_dispositivo = ttk.Label(
-        container, text="Selecione o dispositivo ESP32:")
+    label_dispositivo = ttk.Label(container, text="Selecione o dispositivo ESP32:")
     label_dispositivo.pack(pady=(10, 5))
 
-    combobox_dispositivo = ttk.Combobox(
-        container, values=dispositivos, state="readonly")
-    if dispositivos:
-        combobox_dispositivo.pack(pady=(0, 20))
-        combobox_dispositivo.current(0)
-    else:
-        label_no_device = ttk.Label(
-            container, text="Não existe nenhum dispositivo ESP32 cadastrado", font=("Helvetica", 14))
-        label_no_device.pack(pady=(10, 5))
+    combobox_dispositivo = ttk.Combobox(container, values=dispositivos, state="readonly")
+    combobox_dispositivo.pack(pady=(0, 20))
+    combobox_dispositivo.current(0) if dispositivos else None
 
-    # Configuração da porta serial inicial
-    serial_connection_arduino, _ = open_serial_connection('COM4')
-    if not serial_connection_arduino:
-        messagebox.showerror(
-            "Erro Serial", "Não foi possível abrir a conexão serial na porta COM4.")
-
-    
     # Combobox para seleção de porta serial
     label_port = ttk.Label(container, text="Selecione a porta serial:")
     label_port.pack(pady=(10, 0))
-    port_combobox = ttk.Combobox(container, width=37, values=[
-                                 'COM3', 'COM4', 'COM5'], state="readonly")
+    port_combobox = ttk.Combobox(container, width=37, values=['COM3', 'COM4', 'COM5'], state="readonly")
     port_combobox.pack(pady=10)
     port_combobox.set('COM4')  # Define a porta padrão
-
-    # Atualiza os campos baseado na conexão serial
-    def update_fields():
-        global serial_connection_arduino
-        existing_data = get_existing_data(serial_connection=serial_connection_arduino)
-        can_generate_id = True
-        if existing_data:
-            nome_dispositivo.set(existing_data['nome'])
-            id_var.set(existing_data['id'])
-            can_generate_id = False
-        # entry_id.configure(state='normal' if can_generate_id else 'disabled')
-
-    update_fields()
-
 
     # Monitora mudanças na seleção da porta serial
     def on_port_selected(event):
@@ -83,30 +52,37 @@ def abrir_janela_arduino(root):
         new_connection, success = open_serial_connection(selected_port)
         if success:
             serial_connection_arduino = new_connection
-            update_fields()
-            messagebox.showinfo(
-                "Conexão Serial", f"Conexão estabelecida com sucesso na porta {selected_port}.")
+            messagebox.showinfo("Conexão Serial", f"Conexão estabelecida com sucesso na porta {selected_port}.")
         else:
-            messagebox.showerror(
-                "Conexão Serial", f"Falha ao estabelecer conexão na porta {selected_port}.")
+            messagebox.showerror("Conexão Serial", f"Falha ao estabelecer conexão na porta {selected_port}.")
     port_combobox.bind("<<ComboboxSelected>>", on_port_selected)
+
+    # Botão para buscar dados e atualizar ID e nome
+    def buscar_dados():
+        data = get_existing_data(serial_connection_arduino)
+        if data:
+            nome_dispositivo.set(data['nome'])
+            id_var.set(data['id'])
+            entry_id.config(state='disabled')
+        else:
+            messagebox.showinfo("Aviso", "Dados não encontrados ou dispositivo desconectado.")
+
+    buscar_dados_button = ttk.Button(janela_arduino, text="Buscar Dados", command=buscar_dados)
+    buscar_dados_button.place(relx=0.99, rely=0.01, anchor='ne')
+
     # Interface para nome e ID do dispositivo
     label_nome = ttk.Label(container, text="Nome do dispositivo:")
     label_nome.pack(pady=(10, 5))
-    entry_nome = ttk.Entry(container, width=40, textvariable=nome_dispositivo,)
+    entry_nome = ttk.Entry(container, width=40, textvariable=nome_dispositivo)
     entry_nome.pack(pady=(0, 20))
 
     label_id = ttk.Label(container, text="ID do dispositivo:")
     label_id.pack(pady=(10, 5))
-    entry_id = ttk.Entry(container, width=40, textvariable=id_var)
+    entry_id = ttk.Entry(container, width=40, textvariable=id_var, state='disabled' if id_var.get() else 'normal')
     entry_id.pack(pady=(0, 20))
 
-    btn_acionar = ttk.Button(container, text="Acionar Dispositivo")
-    btn_acionar.pack(pady=(20, 0))
-
     # Botão para voltar à tela principal
-    voltar_button = tk.Button(janela_arduino, text="← Voltar",
-                              command=lambda: fecha_janela.close_and_return(janela_arduino))
-    voltar_button.place(x=10, y=10)
-    janela_arduino.protocol(
-        "WM_DELETE_WINDOW", lambda: fecha_janela.close_and_return(janela_arduino))
+    voltar_button = tk.Button(janela_arduino, text="← Voltar", command=lambda: fecha_janela.close_and_return(janela_arduino))
+    voltar_button.place(relx=0.01, rely=0.01, anchor='nw')
+
+    janela_arduino.protocol("WM_DELETE_WINDOW", lambda: fecha_janela.close_and_return(janela_arduino))
